3eaa841e134da92eb256f6ab065fd164b55816c0
Devon Bedenbaugh <beden2@pdx.edu>
2024-05-26 13:02:03 -0700

    Started implementing a view, attempting to connect it with frontend

--- backend/data_handler_app/views.py
+++ backend/data_handler_app/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from .models import ClientData
 
 # Create your views here.
+
+def client_data_list(request):
+    #This converts a 'QuerySet' to a list of dictionaries. 
+    data = list(ClientData.objects.values())
+
+    return JsonResponse(data, safe=False)
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,53 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios'
+
+
+function App() {
+  //Define component for axios
+  const [data, setData] = useState([]); //
+
+  //Fetch data using axios
+  useEffect(() => {
+    console.log("App.js useEffect called.");
+    axios.get('http://127.0.0.1:8000/clientdata')
+      .then(response => {
+        setData(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
+
+
+  //html loop 'data.map' iterates through object and displays data
+  return (
+    <div className="Handler">
+      <h1>Client Data List</h1>
+      <ul>
+        {data.map(item => (
+          <li key={item.id}>
+            <p>Date: {new DataTransfer(item.date).toLocaleDateString()}</p>
+            <p>Start Time: {item.start_time}</p>
+            <p>Completion Time: {item.completion_time}</p>
+            <p>Language: {item.language}</p>
+            <p>Family Size: {item.family_size}</p>
+            <p>SNAP Benefits: {item.snap_benefits}</p>
+            <p>Travel by Car: {item.travel_by_car}</p>
+            <p>ZIP Code: {item.zip_code}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+
+
+
+}
+
+export default App;
+
+/*
+#import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -23,3 +72,4 @@ function App() {
 }
 
 export default App;
+*/
