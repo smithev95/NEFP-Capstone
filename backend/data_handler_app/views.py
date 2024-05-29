6b83d27a7753e0b1528e3e74254b396343aeef6a
tylerj-code <tylerj.personal@gmail.com>
2024-05-29 16:29:21 -0700

    updated view

--- backend/data_handler_app/views.py
+++ backend/data_handler_app/views.py
@@ -2,10 +2,9 @@ from django.shortcuts import redirect
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-import json
-
 from .forms import ClientDataForm
 from .models import ClientData
+import json
 # Create your views here.
 
 @csrf_exempt
@@ -15,8 +14,21 @@ def client_data_form(request):
         print(data)
     else:
         return JsonResponse({'error': 'not POST request'}, status=400)
-    
-    return JsonResponse({'status': 'API recieved information'})
+        
+    model = ClientData()
+    try: 
+        model.language = data.get('language')
+        model.family_size = data.get('family_size')
+        model.snap_benefits = data.get('snap_benefits')
+        model.travel_by_car = data.get('travel_by_car')
+        model.zip_code = data.get('zip_code')
+        model.save()
+        return JsonResponse({'message': 'successfully submitted'})
+    except:
+        print('not valid JSON data')
+        
+        return JsonResponse({'error': 'not valid JSON data'})  
+      
 
 def client_data_list(request):
     #This converts a 'QuerySet' to a list of dictionaries. 
