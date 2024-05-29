9358ddc5f335f993d53a0a589631e2050af181c4
Evan Smith <smithev@pdx.edu>
2024-05-29 14:03:51 -0700

    Fixed errors

--- backend/data_handler_app/urls.py
+++ backend/data_handler_app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import client_data_list
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('clientdata/', client_data_list, name='client_data_list'),
+    path('', TemplateView.as_view(template_name='index.html'))
 ]
--- backend/data_handler_app/views.py
+++ backend/data_handler_app/views.py
@@ -1,5 +1,6 @@
 
 from django.shortcuts import render
+from django.shortcuts import redirect
 from django.http import JsonResponse
 
 from .forms import ClientDataForm
