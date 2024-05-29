21e83b6d2b5f0caf9cb666863fde99656221815b
tylerj-code <tylerj.personal@gmail.com>
2024-05-29 16:44:35 -0700

    optimized model information fill

--- backend/data_handler_app/views.py
+++ backend/data_handler_app/views.py
@@ -15,23 +15,15 @@ def client_data_form(request):
     else:
         return JsonResponse({'error': 'not POST request'}, status=400)
         
-    model = ClientData()
+    model = ClientData(**data)
     try: 
-        model.language = data.get('language')
-        model.family_size = data.get('family_size')
-        model.snap_benefits = data.get('snap_benefits')
-        model.travel_by_car = data.get('travel_by_car')
-        model.zip_code = data.get('zip_code')
         model.save()
         return JsonResponse({'message': 'successfully submitted'})
     except:
         print('not valid JSON data')
-        
         return JsonResponse({'error': 'not valid JSON data'})  
       
-
 def client_data_list(request):
     #This converts a 'QuerySet' to a list of dictionaries. 
     data = list(ClientData.objects.values())
-
     return JsonResponse(data, safe=False)
