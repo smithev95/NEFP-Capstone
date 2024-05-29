e919dbd44f2f0d105a37622a1b9acdaf9af400c8
Evan Smith <smithev@pdx.edu>
2024-05-29 13:51:06 -0700

    front and backend both launch from django runserver command

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -58,7 +59,9 @@ ROOT_URLCONF = 'backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, '..', 'frontend/build')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -142,3 +145,7 @@ CORS_ALLOWED_ORIGINS = [
 CORS_ALLOW_ALL_ORIGINS = False 
 
 CORS_ALLOW_CREDENTIALS = True
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, '..', 'frontend/build/static'),
+]
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("data_handler_app.urls"))
+    path("", include("data_handler_app.urls")),
+    path('form/', TemplateView.as_view(template_name='index.html')),
+    path('table/', TemplateView.as_view(template_name='index.html')),
 ]
