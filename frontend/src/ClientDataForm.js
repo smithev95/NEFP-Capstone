1d51ed90229dbcfcaf4d5269e396e0c6d219c358
nnha2015 <44039967+nnha2015@users.noreply.github.com>
2024-05-29 22:24:36 -0700

    Apply bootstrap to ClientDataForm

--- frontend/src/ClientDataForm.js
+++ frontend/src/ClientDataForm.js
@@ -1,45 +1,110 @@
-import React from 'react';
+import { Fragment } from 'react';
 
 const ClientDataForm = () => {
     const languageCodes = ['EN', 'ES', 'ZH', 'VI', 'UK', 'RU', 'AR', 'HT', 'FA', 'LO'];
-    const zipCodes = ['97206', '97213', '97216', '97218', '97220', '97230', '97233', '97236', '97266']
+    const zipCodes = ['97206', '97213', '97216', '97218', '97220', '97230', '97233', '97236', '97266'];
+    const yesNo = ['Yes', 'No'];
 
     return (
-        <div>
-            <h1>Client Form</h1>
+        <div class="container">
+            <div class="row mb-2 border-bottom">
+                <h1>Client Form</h1>
+            </div>
             <form method="post">
-                <label htmlFor="language">Language:</label><br />
-                {languageCodes.map((code,index) => (
-                    <React.Fragment key={index}>
-                        <input type="radio" id={`language-${code}`} name="language" value={code} required />
-                        <label htmlFor={`language-${code}`}>{code}</label><br />
-                    </React.Fragment>
-                ))}
-                <label htmlFor="family_size">Family Size:</label><br />
-                {Array.from({ length: 9 }, (_, index) => (
-                    <React.Fragment key={index}>
-                        <input type="radio" id={`family_size-${index + 1}`} name="family_size" value={`family_size-${index + 1}`} required />
-                        <label htmlFor={`family_size-${index + 1}`}>{index + 1}</label><br />
-                    </React.Fragment>
-                ))}
-                <label htmlFor="snap_benefits">SNAP Benefits:</label><br />
-                <input type="radio" id="snap_benefits-yes" name="snap_benefits" value="Yes" required />
-                <label htmlFor="snap_benefits-yes">Yes</label><br />
-                <input type="radio" id="snap_benefits-no" name="snap_benefits" value="No" required />
-                <label htmlFor="snap_benefits-no">No</label><br />
-                <label htmlFor="travel_by_car">Travel by Car:</label><br />
-                <input type="radio" id="travel_by_car-yes" name="travel_by_car" value="Yes" required />
-                <label htmlFor="travel_by_car-yes">Yes</label><br />
-                <input type="radio" id="travel_by_car-no" name="travel_by_car" value="No" required />
-                <label htmlFor="travel_by_car-no">No</label><br />
-                <label htmlFor="zip_code">ZIP Code:</label><br />
-                {zipCodes.map((code, index) => (
-                    <React.Fragment key={index}>
-                        <input type="radio" id={`zip_code-${code}`} name="zip_code" value={`zip_code-${code}`} required />
-                        <label htmlFor={`zip_code-${code}`}>{`${code}`}</label><br />
-                    </React.Fragment>
-                ))}
-                <input type="submit" value="Submit" />
+                <div class="row mb-2 border-bottom">
+                    <div class="col mb-2">
+                        <label class="fs-5 fw-normal" htmlFor="language">Language:</label>
+                        <div class="form-check">
+                            {
+                            languageCodes.map((code, index) => 
+                                (
+                                    <Fragment key={index}>
+                                        <input class="form-check-input" type="radio" id={`language-${code}`} name="language" value={code} required />
+                                        <label class="form-check-label" htmlFor={`language-${code}`}>{code}</label>
+                                        <br />
+                                    </Fragment>
+                                ))
+                            }
+                        </div>
+                    </div>
+                </div>
+                <div class="row mb-2 border-bottom">
+                    <div class="col mb-1">
+                        <label class="fs-5 fw-normal" htmlFor="family_size">Family Size:</label>
+                        <div class="form-check">
+                            {
+                                Array.from({ length: 8 }, (_, index) => 
+                                (
+                                    <Fragment key={index}>
+                                        <input class="form-check-input" type="radio" id={`family_size-${index + 1}`} name="family_size" value={`family_size-${index + 1}`} required />
+                                        <label class="form-check-label" htmlFor={`family_size-${index + 1}`}>{index + 1}</label><br />
+                                    </Fragment>
+                                ))
+                            }
+                        </div>
+                        <div class="form-outline w-25 mb-2">
+                            <input class="form-control" type="text" id="family_size-other" name="family_size" placeholder="Other"/>
+                        </div>
+                    </div>
+                </div>
+                <div class="row mb-2 border-bottom">
+                    <div class="col mb-2">
+                        <label class="fs-5 fw-normal" htmlFor="snap_benefits">SNAP Benefits:</label>
+                        <div class="form-check">
+                            {
+                                yesNo.map((ans, index) => 
+                                    (
+                                        <Fragment key={index}>
+                                            <input class="form-check-input" type="radio" id={`snap_benefits-${ans}`} name="snap_benefits" value={ans} required />
+                                            <label class="form-check-label" htmlFor={`snap_benefits-${ans}`}>{ans}</label>
+                                            <br />
+                                        </Fragment>
+                                    ))
+                            }
+                        </div>
+                    </div>
+                </div>
+                <div class="row mb-2 border-bottom">
+                    <div class="col mb-2">
+                        <label class="fs-5 fw-normal" htmlFor="travel_by_car">Travel by Car:</label><br />
+                        <div class="form-check">
+                                {
+                                    yesNo.map((ans, index) => 
+                                        (
+                                            <Fragment key={index}>
+                                                <input class="form-check-input" type="radio" id={`travel_by_car-${ans}`} name="travel_by_car" value={ans} required />
+                                                <label class="form-check-label" htmlFor={`travel_by_car-${ans}`}>{ans}</label>
+                                                <br />
+                                            </Fragment>
+                                        ))
+                                }
+                        </div>
+                    </div>
+                </div>
+                <div class="row mb-2">
+                    <div class="col mb-2">
+                        <label class="fs-5 fw-normal" htmlFor="zip_code">ZIP Code:</label><br />
+                        <div class="form-check">
+                            {
+                                zipCodes.map((code, index) => 
+                                    (
+                                        <Fragment key={index}>
+                                            <input class="form-check-input" type="radio" id={`zip_code-${code}`} name="zip_code" value={`zip_code-${code}`} required />
+                                            <label class="form-check-label" htmlFor={`zip_code-${code}`}>{`${code}`}</label><br />
+                                        </Fragment>
+                                    ))
+                            }
+                        </div>
+                        <div class="form-outline w-25">
+                            <input class="form-control" type="text" id="family_size-other" name="family_size" placeholder="Other"/>
+                        </div>
+                    </div>
+                </div>
+                <div class="row my-2">
+                    <div class="col mb-2" align="center">
+                        <button class="btn btn-primary btn-lg" type="button">Submit</button>
+                    </div>
+                </div>
             </form>
         </div>
     );
