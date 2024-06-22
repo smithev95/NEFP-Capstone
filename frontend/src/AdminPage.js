d6d266b17f5763c6809dff03367e5bcd53896cb6
Sean Hendrickson <henamber3@aol.com>
2024-06-22 02:17:13 -0700

    Admin page changed from sidebar to navbar

--- frontend/src/AdminPage.js
+++ frontend/src/AdminPage.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import SidebarMenu from './components/sidebar.js';
+import NavbarMenu from './components/Navbar.js';
 
 const AdminPage = () => {
     return (
-        <SidebarMenu />
+        //<SidebarMenu />
+        <NavbarMenu />
     );
 }
 
---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,62 @@
+import React from "react";
+import { Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import CompanyLogo from "../img/companyLogo50Year.png";
+
+// source: https://react-bootstrap.netlify.app/docs/components/navbar/
+function NavbarMenu() {
+    return (
+        <Navbar expand="lg" className="bg-body-tertiary">
+            <Container>
+                <Navbar.Brand href="#home">
+                    <img 
+                        src={CompanyLogo}
+                        width="80"
+                        height="39"
+                        className="d-inline-block aligh-top"
+                        alt="NEFP logo"
+                    />
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="me-auto">
+                    <NavDropdown title="Editor" id="basic-nav-dropdown">
+                        <NavDropdown.Item href="#action/1.1">Adjust Questionnaire</NavDropdown.Item>
+                        <NavDropdown.Divider />
+                        <NavDropdown.Item href="#action/1.2">Add Question</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/1.3">Update Question</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/1.4">Delete Question</NavDropdown.Item>
+                        <NavDropdown.Divider />
+                        <NavDropdown.Item href="#action/1.5">Add Language</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/1.6">Remove Language</NavDropdown.Item>
+                    </NavDropdown>
+                    <NavDropdown title="Dataviewer" id="basic-nav-dropdown">
+                        <NavDropdown.Item href="#action/2.1">Custome Data Viewer</NavDropdown.Item>
+                        <NavDropdown.Divider />
+                        <NavDropdown.Item href="#action/2.2">Client Data</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/2.3">Languages</NavDropdown.Item>
+                    </NavDropdown>
+                    <NavDropdown title="Forms" id="basic-nav-dropdown">
+                        <NavDropdown.Item href="#action/3.1">View Form 1</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.2">View Form 2</NavDropdown.Item>
+                    </NavDropdown>
+                    <NavDropdown title="Settings" id="basic-nav-dropdown">
+                        <NavDropdown.Item href="#action/4.1">Setting 1</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/4.2">Setting 2</NavDropdown.Item>
+                        <NavDropdown.Divider />
+                        <NavDropdown.Item href="#action/4.3">Sign-out</NavDropdown.Item>
+                    </NavDropdown>
+                </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+}
+
+export default NavbarMenu;
