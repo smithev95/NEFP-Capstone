6f78390bb14045b1c0168f69502f6839e1d3cd2f
nnha2015 <44039967+nnha2015@users.noreply.github.com>
2024-07-06 08:31:31 -0700

    Remove unused import and add addQuestion to Navbar

--- frontend/src/components/LogoNavbar.js
+++ frontend/src/components/LogoNavbar.js
@@ -1,11 +1,8 @@
 import React from "react";
-import { Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 
 import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 import CompanyLogo from "../img/companyLogo50Year.png";
 
 // source: https://react-bootstrap.netlify.app/docs/components/navbar/
