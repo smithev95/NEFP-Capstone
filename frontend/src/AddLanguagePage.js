7530f8c4cd445bb3df42fb7795a2a8c1effe7fb3
Sean Hendrickson <henamber3@aol.com>
2024-08-17 19:02:10 -0700

    moved AddLanguagePage.js file

---
+++ frontend/src/AddLanguagePage.js
@@ -0,0 +1,8 @@
+// select language
+// make sure language was not already added
+// load all questions
+// translate all questions
+// add language and question translations to the database
+// inform user whether data was added sucessfully
+    // if failed, display failure + reason, allow user to edit form
+    // if success, display success and refresh page
